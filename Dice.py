62c4d4561010307c2f2344f01affb1b86a247a18
Slober3 <quintendesmyter@hotmail.com>
2018-07-27 02:07:58 +0200

    Proof system
    Added a dice roll validator in the code

--- Dice.py
+++ Dice.py
@@ -2,6 +2,7 @@ import json
 import argparse
 import random
 from time import time
+from hashlib import sha256
 from flask import Flask, jsonify, request, render_template
 
 parser = argparse.ArgumentParser(description="Command line arguments")
@@ -11,20 +12,44 @@ args = parser.parse_args()
 def dice_gen(number):
     return random.randint(1, number)
 
+def dice_proof(number,dice_time):
+    y = 0
+    while sha256(f'{number}{y}{dice_time}'.encode()).hexdigest()[:5] != "00000":
+        y += 1
+    return y
+
+def dice_validate_proof(proof,result,dice_time):
+    return sha256(f'{result}{proof}{dice_time}'.encode()).hexdigest()[:5] == "00000"
+
 app = Flask(__name__)
 @app.route('/dice/json/<int:number>', methods=['GET'])
 def json_dice(number):
+    result = dice_gen(number)
+    dice_time = time()
     response = {
         'message': "New dice roll ",
         'type': "d%s" %number,
-        'result': dice_gen(number),
-        'timestamp': time(),
+        'result': result,
+        'proof' : dice_proof(result,dice_time),
+        'timestamp': dice_time,
     }
     return jsonify(response), 200
 
 @app.route('/dice/<int:number>', methods=['GET'])
 def dice(number):
     return render_template('dice.html', number=number,result=dice_gen(number))
+
+@app.route('/validate/<int:result>/<int:proof>/<float:dice_time_r>', methods=['GET'])
+def dice_validate(proof,result,dice_time_r):
+    result = dice_validate_proof(result,proof,dice_time_r)
+    dice_time = time()
+    response = {
+        'message': "Validate",
+        'type': result,
+        'timestamp': dice_time,
+    }
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(args.p))
